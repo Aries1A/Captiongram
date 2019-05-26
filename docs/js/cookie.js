f96d846163a6a00d87b02ec76b5d34b16169353c
Aries1A <kaiso.mar26.littleswitch@gmail.com>
2019-05-27 02:28:58 +0900

    [update] cookie導入

--- docs/js/cookie.js
+++ docs/js/cookie.js
@@ -1,15 +1,6 @@
 $(function() {
-  console.log($.cookie("Datecheck"));
-  console.log($.cookie("Cameracheck"));
-  console.log($.cookie("Lenscheck"));
-  console.log($.cookie("Lengthcheck"));
-  console.log($.cookie("Timecheck"));
-  console.log($.cookie("FNumbercheck"));
-  console.log($.cookie("ISOcheck"));
-
-
   $(document).on("click", "#Datecheck", function() {
-    let Datechecked = $("#Datecheck").get(0).checked;
+    var Datechecked = $("#Datecheck").get(0).checked;
     console.log(Datechecked);
     $.cookie("Datecheck", Datechecked, {
       expires: 730
@@ -17,12 +8,12 @@ $(function() {
     console.log($.cookie("Datecheck"));
   });
   const _Datecheck = $.cookie("Datecheck");
-  if (_Datecheck === "false") {
+  if (_Datecheck == "false") {
     $("#Datecheck").attr("checked", false);
   }
 
   $(document).on("click", "#Cameracheck", function() {
-    let Camerachecked = $("#Cameracheck").get(0).checked;
+    var Camerachecked = $("#Cameracheck").get(0).checked;
     console.log(Camerachecked);
     $.cookie("Cameracheck", Camerachecked, {
       expires: 730
@@ -30,12 +21,12 @@ $(function() {
     console.log($.cookie("Cameracheck"));
   });
   const _Cameracheck = $.cookie("Cameracheck");
-  if (_Cameracheck === "false") {
+  if (_Cameracheck == "false") {
     $("#Cameracheck").attr("checked", false);
   }
 
   $(document).on("click", "#Lenscheck", function() {
-    let Lenschecked = $("#Lenscheck").get(0).checked;
+    var Lenschecked = $("#Lenscheck").get(0).checked;
     console.log(Lenschecked);
     $.cookie("Lenscheck", Lenschecked, {
       expires: 730
@@ -43,12 +34,12 @@ $(function() {
     console.log($.cookie("Lenscheck"));
   });
   const _Lenscheck = $.cookie("Lenscheck");
-  if (_Lenscheck === "false") {
+  if (_Lenscheck == "false") {
     $("#Lenscheck").attr("checked", false);
   }
 
   $(document).on("click", "#Lengthcheck", function() {
-    let Lengthchecked = $("#Lengthcheck").get(0).checked;
+    var Lengthchecked = $("#Lengthcheck").get(0).checked;
     console.log(Lengthchecked);
     $.cookie("Lengthcheck", Lengthchecked, {
       expires: 730
@@ -56,12 +47,12 @@ $(function() {
     console.log($.cookie("Lengthcheck"));
   });
   const _Lengthcheck = $.cookie("Lengthcheck");
-  if (_Lengthcheck === "false") {
+  if (_Lengthcheck == "false") {
     $("#Lengthcheck").attr("checked", false);
   }
 
   $(document).on("click", "#Timecheck", function() {
-    let Timechecked = $("#Timecheck").get(0).checked;
+    var Timechecked = $("#Timecheck").get(0).checked;
     console.log(Timechecked);
     $.cookie("Timecheck", Timechecked, {
       expires: 730
@@ -69,12 +60,12 @@ $(function() {
     console.log($.cookie("Timecheck"));
   });
   const _Timecheck = $.cookie("Timecheck");
-  if (_Timecheck === "false") {
+  if (_Timecheck == "false") {
     $("#Timecheck").attr("checked", false);
   }
 
   $(document).on("click", "#FNumbercheck", function() {
-    let FNumberchecked = $("#FNumbercheck").get(0).checked;
+    var FNumberchecked = $("#FNumbercheck").get(0).checked;
     console.log(FNumberchecked);
     $.cookie("FNumbercheck", FNumberchecked, {
       expires: 730
@@ -82,12 +73,12 @@ $(function() {
     console.log($.cookie("FNumbercheck"));
   });
   const _FNumbercheck = $.cookie("FNumbercheck");
-  if (_FNumbercheck === "false") {
+  if (_FNumbercheck == "false") {
     $("#FNumbercheck").attr("checked", false);
   }
 
   $(document).on("click", "#ISOcheck", function() {
-    let ISOchecked = $("#ISOcheck").get(0).checked;
+    var ISOchecked = $("#ISOcheck").get(0).checked;
     console.log(ISOchecked);
     $.cookie("ISOcheck", ISOchecked, {
       expires: 730
@@ -95,7 +86,7 @@ $(function() {
     console.log($.cookie("ISOcheck"));
   });
   const _ISOcheck = $.cookie("ISOcheck");
-  if (_ISOcheck === "false") {
+  if (_ISOcheck == "false") {
     $("#ISOcheck").attr("checked", false);
   }
 
