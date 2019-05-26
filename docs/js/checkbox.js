e39be3a00c14eb80f36070de79cbf79707bc2f0f
Aries1A <kaiso.mar26.littleswitch@gmail.com>
2019-05-27 01:50:17 +0900

    [update] cookie導入

--- docs/js/checkbox.js
+++ docs/js/checkbox.js
@@ -1,15 +1,15 @@
 $(function(){
 
-  var checkAll = '#checkAll'; //「すべて」のチェックボックスのidを指定
-  var checkBox = 'input[name="area[]"]'; //チェックボックスのnameを指定
+  const checkAll = '#checkAll'; //「すべて」のチェックボックスのidを指定
+  const checkBox = 'input[name="area[]"]'; //チェックボックスのnameを指定
 
   $( checkAll ).on('click', function() {
     $( checkBox ).prop('checked', $(this).is(':checked') );
   });
 
   $( checkBox ).on( 'click', function() {
-    var boxCount = $( checkBox ).length; //全チェックボックスの数を取得
-    var checked  = $( checkBox + ':checked' ).length; //チェックされているチェックボックスの数を取得
+    const boxCount = $( checkBox ).length; //全チェックボックスの数を取得
+    const checked  = $( checkBox + ':checked' ).length; //チェックされているチェックボックスの数を取得
     if( checked === boxCount ) {
       $( checkAll ).prop( 'checked', true );
     } else {
