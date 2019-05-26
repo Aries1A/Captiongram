ea78ee9b3bb4ec4ca27e8404beb21ea702d965b9
Aries1A <kaiso.mar26.littleswitch@gmail.com>
2019-05-27 01:07:06 +0900

    [update] cookie導入

--- docs/js/cookie.js
+++ docs/js/cookie.js
@@ -1,16 +1,16 @@
 $(function(){
 
-//#newtweetcheckをクリック時に値をクッキーに格納
-$(document).on("click","#newtweetcheck", function(){
-     let checked=$("#newtweetcheck").get(0).checked;
+//checkAllをクリック時に値をクッキーに格納
+$(document).on("click","checkAll", function(){
+     let checked=$("checkAll").get(0).checked;
      console.log(checked);
      $.cookie("newtweet", checked,{ expires: 730 });
      console.log($.cookie("newtweet"));
 });
-//ページを読み込んだ時に#newtweetcheckの値を設定
+//ページを読み込んだ時にcheckAllの値を設定
      const _newtweet=$.cookie("newtweet");
      if(_newtweet != "false"){
-     $("#newtweetcheck").attr("checked",true);
+     $("checkAll").attr("checked",true);
      }
 
 });
