6152f7cc79a2e71e93fb53640e1ae3e9d629995d
Aries1A <kaiso.mar26.littleswitch@gmail.com>
2019-05-27 08:42:02 +0900

    [update] クリップボードにコピーを分割

--- docs/js/copytext.js
+++ docs/js/copytext.js
@@ -1,9 +1,19 @@
 function copyToClipboard() {
-  var yourCode = document.getElementById('textExif');
+  var yourCode = document.getElementById('textExif1');
   var range = document.createRange();
   range.selectNode(yourCode);
   window.getSelection().removeAllRanges();
   window.getSelection().addRange(range);
   document.execCommand('copy');
   alert('コピーしました');
-        }
+}
+
+function copyToClipboard() {
+  var yourCode = document.getElementById('textExif2');
+  var range = document.createRange();
+  range.selectNode(yourCode);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  document.execCommand('copy');
+  alert('コピーしました');
+}
